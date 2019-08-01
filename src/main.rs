12eda597274ee1726f6157e0a018ffac78475379
glitchy <v.smit.mail@gmail.com>
2019-08-01 21:00:47 +0200

    First version with graphics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#[macro_use]
+extern crate nom;
+extern crate rand;
+extern crate glium;
+
+#[allow(unused_imports)]
+
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::{fmt::Write, num::ParseIntError};
-use std::sync::Arc;
-use std::cell;
+use std::sync::mpsc::channel;
+use std::thread::*;
 
-extern crate nom;
-use nom::{HexDisplay,IResult,Needed,Offset};
-
+use rand::Rng;
+use glium::{glutin, Surface, Display};
+use glium::index::PrimitiveType;
 
 
 
@@ -16,28 +24,105 @@ fn main() {
     let filename = r"roms/Fishie.ch8";
     let mut rom_file = File::open(filename).unwrap();
     let mut binary = Vec::new();
-    let mut length = rom_file.read_to_end(&mut binary).unwrap();
+    let length = rom_file.read_to_end(&mut binary).unwrap();
 
-    let mut count = 100;    //let hex_e = decode_hex("e0").unwrap();
-    //let hex_d = decode_hex("d0").unwrap();
-    //println!("{:?}", hex_d);
-    
     let mut cpu = CPU::new(binary);
+    let (mut eventloop, mut screen) = Screen::new();
 
-    loop {
+    let (sender, receiver) = channel();
 
+    //TODO: Move rom binary to thread
+    let cputhread = spawn(move || {
+        let mut count = 100;
+        loop {
 
-        //let instr = cpu.fetch_opcode().unwrap();
-        cpu.parse_instruction();
-        //state.cpu_cycle(opcode);
+            // return result, if result we pass vec[u8] as data to refresh the screen by passing the instruction to main thread.
+            match cpu.parse_instruction() {
+                Some(instr) => {
+                    if sender.send(instr).is_err() {
+                    break
+                    }
+                }
+                None => {
+                    println!("No GPU instructions");
+                }
+            }
 
-        count +=1;
-        if count >= length {
-            break
+            count +=1;
+            if count >= length {
+                break
+            }
         }
+    });
+
+    // Start Glium eventloop
+
+    let mut closed = false;
+    while !closed {
+        eventloop.poll_events(|eventloop|  {
+            match eventloop {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::CloseRequested => closed = true,
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
     }
 
+    cputhread.join();
 }
+
+
+struct Keyboard {
+    hex: u8
+}
+
+struct Sound {
+    hex: u8
+}
+
+struct Screen {
+    x: u8,
+    y: u8,
+}
+
+#[derive(Default)]
+struct RenderOptions {
+    pub linear_interpolation: bool,
+}
+
+
+
+impl Screen {
+
+    fn new() -> (glium::glutin::EventsLoop, glium::backend::glutin::Display) {
+        let mut eventsloop = glium::glutin::EventsLoop::new();
+        let window_builder = glium::glutin::WindowBuilder::new()
+            .with_dimensions(glium::glutin::dpi::LogicalSize::from((64, 32)))
+            .with_title("Chippy8 - ".to_owned());
+        let context_builder = glium::glutin::ContextBuilder::new();
+        let display = glium::backend::glutin::Display::new(window_builder, context_builder, &eventsloop).unwrap();
+
+        let mut texture = glium::texture::texture2d::Texture2d::empty_with_format(
+                &display,
+                glium::texture::UncompressedFloatFormat::U8U8U8,
+                glium::texture::MipmapsOption::NoMipmap,
+                64 as u32,
+                32 as u32)
+            .unwrap();
+        (eventsloop, display)
+    }
+
+    fn refresh(&mut self,
+        texture: &mut glium::texture::texture2d::Texture2d,
+        datavec: &[u8],
+        renderoptions: &RenderOptions
+        ) {
+        println!("Hello");;
+    }
+}
+
 
 struct CPU {
     memory: Vec<u8>,
@@ -45,12 +130,9 @@ struct CPU {
     sound_register: u8,
     delay_register: u8,
     pc: usize,
-    sp: u8,
-    stack: Vec<u8>,
+    sp: usize,
+    stack: Vec<usize>,
     i: u8,
-    //display: Display,
-    //sound: Sound,
-    //keyboard: Keyboard,
 }
 
 
@@ -58,115 +140,117 @@ impl CPU {
 
     fn new(binary: Vec<u8>) -> CPU {
 
-    let mut memory: Vec<u8> = Vec::with_capacity(4096);
-    let memory = binary.iter().enumerate().map(move|(i,x)| if i == 500 {0} else {*x}).collect();
+        let mut memory: Vec<u8> = Vec::with_capacity(4096);
 
-    println!("{:?}", memory);
+        let mem_start: u16 = 512;
+        let value = 0;
 
-    let mut register = Vec::with_capacity(16);
-    for nr in 1..16 {
-        register.push(nr);
-    };
-    let mut stack = Vec::with_capacity(16);
-
-    for nr in 1..16 {
-        stack.push(nr);
-    }
-
-
-    CPU {
-            memory : memory,
-            register : register,
-            sound_register : 0,
-            delay_register : 0,
-            pc : 0,
-            sp : 0,
-            stack : stack,
-            i : 0,
+        for i in 0..mem_start {
+            memory.push(value.clone());
         }
+
+        for (i, hex) in binary.iter().enumerate() {
+            memory.push(hex.clone())
+        }
+        println!("{:?}", memory);
+
+        let mut register = Vec::with_capacity(16);
+        for nr in 1..16 {
+            register.push(nr);
+        };
+        let mut stack = Vec::with_capacity(16);
+
+        for nr in 1..16 {
+            stack.push(nr);
+        }
+
+
+        CPU {
+                memory : memory,
+                register : register,
+                sound_register : 0,
+                delay_register : 0,
+                pc : 0x200,
+                sp : 0,
+                stack : stack,
+                i : 0,
+            }
     }
     
-    fn fetch_opcode<'a>(self: Self) -> Option< Vec<u8>> {
+    fn fetch_opcode<'a>(self: Self) -> Option<Vec<u8>> {
         println!("Fetching opcode..");
-
-        let nibble1 = (self.memory[self.pc] & 0x00F0) >> 4;
-        let nibble2 = self.memory[self.pc] & 0x000F; 
-        let nibble3 = (self.memory[self.pc+1] & 0x00F0) >> 4;
-        let nibble4 = self.memory[self.pc+1] & 0x000F;
-
         let mut content = Vec::new();
-        content.push(nibble1);
-        content.push(nibble2);
-        content.push(nibble3);
-        content.push(nibble4);
-
         return Some(content)
     }
 
     fn cpu_cycle(self) {
         // 60times a second, grab an opcode and execute the comment
-
         // process_opcode()
     }
 
 
 
-    fn parse_instruction(&mut self) {
-
-        //let nibble1 = (self.memory[self.pc] & 0x00F0) >> 4;
-        //let nibble2 = self.memory[self.pc] & 0x000F; 
+    fn parse_instruction(&mut self) -> Option<Vec<u8>> {
 
         let left: u16 = self.memory[self.pc].clone().into();
         let right: u16 = self.memory[self.pc + 1].clone().into();
-        println!("{:x}", left);
-        println!("{:x}", right);
 
-        let opcode: u16 = left | right;
+        //println!("{:b}", left);
+        //println!("{:b}", right);
+        //println!("{:x}", left);
+        //println!("{:x}", right);
 
-        println!("First nibble.. {:x}", opcode & 0xF000);
+        let opcode: u16 = (left << 8| right) as u16;
+
         println!("Parsing.. {:x}",opcode);
+        println!("Parsing.. {:b}",opcode);
+        println!("First nibble '{:b}', second nibble '{:b}', third nibble '{:b}', fourth nibble '{:b}'", opcode & 0xFFFF,opcode & 0x0FFF, opcode & 0x00FF, opcode & 0x0F );
+        println!("First nibble '{:x}', second nibble '{:x}', third nibble '{:x}', fourth nibble '{:x}'", opcode & 0xFFFF,opcode & 0x0FFF, opcode & 0x00FF, opcode & 0x0F );
 
-        let mut instr = Vec::new();
-        instr.push(1);
-        instr.push(2);
-        instr.push(3);
-        instr.push(4);
-
-        
-        match instr[0] {
-            0 => {
-                match instr[3] {
+        println!("{}", opcode & 0xF000);
+        match opcode & 0xF000 {
+            0x0000 => {
+                match opcode & 0xF {
                     0 => {
-                        println!("Clear screen")
+                        println!("Clear screen");
+                        self.pc += 2;
                     }
                     _ => {
-                        println!("Returns from a subroutine")
+                        println!("Returns from a subroutine");
+                        self.pc =  self.stack[self.sp]
                     }
                 }
             }
 
-            1 => {
+            0x1000 => {
                 println!("JUMP NNN");
-                let target: u8 = instr.iter().skip(1).sum();
+                self.stack[self.sp] = self.pc;
+                self.sp += 1;
+                let target = opcode & 0xFFF;
                 self.pc = target as usize;
             }
 
-            2 => {
-                println!("Calls subroutine at NNN")
+            0x2000 => {
+                println!("Calls subroutine at NNN");
+                self.stack[self.sp] = self.pc;
+                self.sp += 1;
+                self.pc = (opcode & 0x0FFF) as usize;
+
             }
 
-            3 => {
-                if self.register[instr[1] as usize] == instr.iter().skip(2).sum() {
+            0x3000 => {
+                if self.register[((opcode & 0xF00) >> 8) as usize] == (opcode & 0xFF) as u8 {
                     println!("SKIP.EQ");
-                    self.pc += 2;
+                    self.pc += 4;
                 }
                 else {
-                    println!("NOT SKIP.EQ")
+                    println!("NOT SKIP.EQ");
+                    self.pc += 2;
                 }               
             }
             
-            4 => {
-                if self.register[instr[1] as usize] != instr.iter().skip(2).sum() {
+            0x4000 => {
+                if self.register[((opcode & 0xF00) >> 8) as usize] != (opcode & 0xFF) as u8 {
                     println!("SKIP.NE");
                     self.pc += 2;
                 }
@@ -175,68 +259,91 @@ impl CPU {
                 }
             }
 
-            5 => {
-                if self.register[instr[1] as usize] == self.register[instr[2] as usize] {
+            0x5000 => {
+                if self.register[((opcode & 0xF00) >> 8) as usize] == self.register[((opcode & 0xF0) >> 4) as usize] {
                     println!("SKIP.EQ");
-                    self.pc += 2;
+                    self.pc += 4;
                 }
                 else {
-                    println!("NOT SKIP.EQ")
+                    println!("NOT SKIP.EQ");
+                    self.pc += 2;
                 } 
             }
             
-            6 => {
-                self.register[instr[1] as usize] = instr.iter().skip(2).sum();
-                println!("MVI.");
+            0x6000 => {
+                println!("{}", self.register[((opcode & 0x0F00) >>8) as usize]);
+                self.register[((opcode & 0xF00) >> 8) as usize] = (opcode & 0xFF) as u8;
+                println!("Vx = NN");
+                self.pc += 2;
             }
 
-            7 => {
-                let count = self.register[instr[1] as usize];
-                let count2: u8 =  instr.iter().skip(2).sum();
-                self.register[instr[1] as usize] = count + count2;
-                println!("Adds NN to VX")
+            0x7000 => {
+                self.register[((opcode & 0xF00) >> 8) as usize] = (opcode & 0xFF) as u8;
+                println!("Adds NN to VX");
+                self.pc += 2;
+
             }
 
-            8 => {
-                println!("MOD");
-                match instr[3] {
+            0x8000 => {
+                self.pc += 2;
+                match opcode & 0xF {
                     0 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[1] as usize] = self.register[instr[2] as usize];
+                        println!("Vx=Vy");
+                        self.register[((opcode & 0xF00) >> 8) as usize] = self.register[((opcode & 0xF0) >> 4) as usize];
                     }
 
                     1 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[1] as usize] = self.register[instr[2] as usize] | self.register[instr[2] as usize];
+                        println!("Vx=Vx|Vy");
+                        self.register[((opcode & 0xF00) >> 8) as usize] = self.register[((opcode & 0xF00) >> 8) as usize] | self.register[((opcode & 0xF0) >> 4) as usize];
                     }
 
                     2 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[1] as usize] = self.register[instr[2] as usize] | self.register[instr[2] as usize];
+                        println!("Vx=Vx&Vy");
+                        self.register[((opcode & 0xF00) >> 8) as usize] = self.register[((opcode & 0xF00) >> 8) as usize] & self.register[((opcode & 0xF0) >> 4) as usize];
                     }
                     3 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[1] as usize] = self.register[instr[2] as usize] ^ self.register[instr[2] as usize];
+                        println!("Vx=Vx^Vy");
+                        self.register[((opcode & 0xF00) >> 8) as usize] = self.register[((opcode & 0xF00) >> 8) as usize] ^ self.register[((opcode & 0xF0) >> 4) as usize];
                     }
                     4 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[2] as usize] += self.register[instr[2] as usize];
+                        println!("Vx += Vy");
+                        if self.register[(opcode & 0x00F0) as usize  >> 4] > (0xFF - self.register[(opcode & 0x0F00) as usize >> 8]) {
+                            self.register[0xF] = 1; //carry
+                        }
+                        else {
+                            self.register[0xF] = 0;
+                            self.register[(opcode & 0x0F00) as usize >> 8] += self.register[(opcode & 0x00F0) as usize >> 4];
+                        }
                     }
                     5 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[2] as usize] -= self.register[instr[2] as usize];
+                        println!("Vx += Vy");
+                        let borrow = self.register[(opcode & 0x0F00) as usize] - self.register[(opcode & 0x00F0) as usize];
+                        if borrow > 0 {
+                            self.register[0xF] = 0; //carry
+                        }
+                        else {
+                            self.register[0xF] = 1;
+                        }
                     }
                     6 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[2] as usize] >>= self.register[instr[2] as usize];
+                        println!("{:b}",opcode & 0x0F00 >> 1);
+                        println!("Vx>>=1");
+                        self.register[0xF] = self.register[(opcode & 0x0F00) as usize]  >>1
                     }
                     7 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[1] as usize] = self.register[instr[2] as usize] ^ self.register[instr[1] as usize];
+                        println!("Vx=Vy-Vx");
+                        let borrow = self.register[(opcode & 0x0F00) as usize] - self.register[(opcode & 0x00F0) as usize];
+                        if borrow > 0 {
+                            self.register[0xF] = 0; //carry
+                        }
+                        else {
+                            self.register[0xF] = 1;
+                        }
                     }
                     14 => {
-                        println!("Sets VX to the value of VY.");
-                        self.register[instr[2] as usize] >>= self.register[instr[2] as usize];
+                        println!("{:b}",opcode & 0x0F00 << 1);
+                        println!("Vx<<=1");
+                        self.register[0xF] = self.register[(opcode & 0x0F00) as usize]  <<1
                     } 
                     _ => {
                         println!("Else")
@@ -244,38 +351,53 @@ impl CPU {
                 }
             }
             
-            9 => {
-                if self.register[instr[1] as usize] != self.register[instr[2] as usize] {
-                    println!("if(Vx!=Vy)");
-                    self.pc += 2;
+            0x9000 => {
+                println!("if(Vx!=Vy)");
+                if self.register[((opcode & 0xF00) >> 8) as usize] != self.register[((opcode & 0xF0) >> 4) as usize] {
+                    self.pc += 4;
                 }
             }
             
-            10 => {
+            0xA000 => {
                 println!("I = NNN");
-                let mem_loc: u8 = instr.iter().skip(1).sum();
-                println!("{}", mem_loc);
-                self.i = self.memory[mem_loc as usize];
+                println!("{:x}",opcode & 0xFFF);
+                self.i = (opcode & 0xFFF) as u8;
+                self.pc += 2;
 
             }
 
-            11 => {
+            0xB000 => {
                 println!("PC=V0+NNN");
-                let target: u8 = instr.iter().skip(1).sum();
-                let target = target + self.register[0];
+                let target = (opcode & 0xFFF) as u8 + self.register[0x0];
                 self.pc = target as usize;
+                self.pc += 2;
+
             }
             
-            12 => {
-                println!("Vx=rand()&NN")
+            0xC000 => {
+                self.register[((opcode & 0xF00) >> 8) as usize] = (rand::thread_rng().gen_range(0, 255) as u8) & (opcode & 0xFF) as u8;
+                println!("Vx=rand()&NN");
+                self.pc += 2;
+
             }
             
-            13 => {
-                println!("draw(Vx,Vy,N)")
+            0xD000 => {
+
+                let x = self.register[((opcode & 0x0F00) >> 8) as usize];
+                let y = self.register[((opcode & 0x00F0) >> 4) as usize];
+                let height = (opcode & 0x000F);
+
+                self.register[0xF] = 0;
+                println!("draw(Vx,Vy,N)");
+                self.pc += 2;
+
+                let result: Vec<u8> = Vec::new();
+                Some(result);
             }
 
-            14 => {
-                match instr[3] {
+            0xE000 => {
+                self.pc += 2;
+                match opcode & 0xF {
                     14 => {
                         println!("if(key()==Vx)")
                     }
@@ -289,8 +411,9 @@ impl CPU {
                 }
             }
 
-            15 => {
-                match (instr[2], instr[3]) {
+            0xF000 => {
+                self.pc += 2;
+                match (opcode & 0xFF, opcode & 0xF) {
                     (0,7) => {
                         println!("Vx = get_delay()	")
                     },
@@ -326,39 +449,9 @@ impl CPU {
             _ => {
                 println!("Everything else")
             }
-
         }
-        self.pc += 2;
+        None
 
     }
 
-}
-
-struct Display {
-    x: u8,
-    y: u8,
-}
-
-struct Keyboard {
-    hex: u8
-}
-
-struct Sound {
-    hex: u8
-}
-
-
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
-}
-
-pub fn encode_hex(bytes: &[u8]) -> String {
-    let mut s = String::with_capacity(bytes.len() * 2);
-    for &b in bytes {
-        write!(&mut s, "{:02x}", b);
-    }
-    s
 }
